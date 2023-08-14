4e4a8bd4d751daf40efcb417220530eb0ff78dc3
meet_hansalia <meethansalia99@gmail.com>
2023-08-14 18:19:55 +0530

    fetch

--- shopmate/src/App.js
+++ shopmate/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import ProductList from './components/ProductList';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Product List</h1>
+      <ProductList/>
     </div>
   );
 }
